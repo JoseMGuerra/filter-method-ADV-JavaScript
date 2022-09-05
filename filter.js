846e9a75d2e550ba767e570ba2619c39d2375437
JoseMGuerra <Josem_guerra@hotmail.com>
2022-09-05 14:50:31 +0000

    filter method ADV JavaScript lesson

--- filter.js
+++ filter.js
@@ -1,9 +1,7 @@
-  
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node filter.js in the terminal
  */
-
 
 // Simple Filtering
 const people = [
@@ -20,7 +18,14 @@ const people = [
     age: 18,
   },
 ];
+const oldEnough = people.filter(people => people.age >= 21);
+console.log(oldEnough);
 
+const paul = people.filter(p => p.name == 'Paul');
+console.log(paul);
+
+const paulObj = people.filter(p => p.name == 'Paul')[0];
+console.log(paulObj);
 
 // Complex Filtering
 const students = [
@@ -32,7 +37,7 @@ const students = [
       { name: 'javascript', yrsExperience: 1 },
       { name: 'html', yrsExperience: 5 },
       { name: 'css', yrsExperience: 3 },
-    ]
+    ],
   },
   {
     id: 2,
@@ -42,7 +47,7 @@ const students = [
       { name: 'javascript', yrsExperience: 0 },
       { name: 'html', yrsExperience: 4 },
       { name: 'css', yrsExperience: 2 },
-    ]
+    ],
   },
   {
     id: 3,
@@ -52,6 +57,30 @@ const students = [
       { name: 'javascript', yrsExperience: 1 },
       { name: 'html', yrsExperience: 1 },
       { name: 'css', yrsExperience: 5 },
-    ]
+    ],
   },
 ];
+// more difficult to read
+
+// const candidates = students.filter(student => {
+//   const strongSkills = student.skills.filter(
+//     skills => skills.yrsExperience >= 5
+//   );
+//   return strongSkills.length > 0;
+// });
+
+// a more easy to read and elegant approach
+
+const has5YrsExp = skills => skills.yrsExperience >= 5;
+const hasStrongSkills = student => {
+  const strongSkills = student.skills.filter(has5YrsExp);
+  return strongSkills.length > 0;
+};
+const candidates = students.filter(hasStrongSkills);
+
+console.log(candidates);
+
+// extract and print only candidates names
+const candidatesNames = candidates.map(candidate => candidate.name);
+
+console.log('Candidates:', candidatesNames);
